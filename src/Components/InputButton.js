bf36859f0aafd775a328cc771c80dd0e1f2550c7
Konstantin <kostaromand@gmail.com>
2019-08-09 14:37:29 +0600

    Word-break in card header in column changed

--- src/Components/InputButton.js
+++ src/Components/InputButton.js
@@ -4,7 +4,7 @@ export default class InputButton extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: ""
+            value: props.initialValue
         }   
     }
 
